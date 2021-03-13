86c583e84a217e1c4a98726edb7920efd2da6f0b
SophonTBP <feidmahnkassad@gmail.com>
2021-03-13 23:55:08 +0100

    images lazy loading

--- observers.js
+++ observers.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home-intro");
 
+const faders = document.querySelectorAll('.fade-in');
+const sliders =document.querySelectorAll('.slide-in')
 const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px"
 };
@@ -20,3 +22,65 @@ const sectionOneObserver = new IntersectionObserver(function(
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold:0,
+  rootMargin:"0px 0px -400px 0px"
+};
+
+
+const appearOnscroll = new IntersectionObserver(
+  function(
+    entries, 
+    appearOnScroll
+    ){
+      entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+          return;
+        } else {
+          entry.target.classList.add('appear');
+          appearOnScroll.unobserve(entry.target);
+        }
+      })
+    }, 
+    appearOptions);
+
+faders.forEach(fader =>{
+  appearOnscroll.observe(fader);
+});
+
+sliders.forEach(slider =>{
+  appearOnscroll.observe(slider);
+});
+
+
+//Lazy loading//
+
+
+const images = document.querySelectorAll("[data-src]");
+function preloadImage(img){
+  const src = img.getAttribute("data-src");
+  if(!src){
+    return;
+  }
+  img.src = src;
+}
+ const imgOptions = {
+ threshold:1,
+ rootMargin:"0px 0px -500px 0px"
+
+ };
+ const imgObserver = new IntersectionObserver((imgObserver) => {
+   entries.forEach(entry => {
+     if(!entry.isIntersecting){
+       return
+     } else {
+       preloadImage(entry.target);
+       imgObserver.unobserve(entry.target);
+     }
+   })
+ }, imgOptions);
+
+ images.forEach(image => {
+   imgObserver.observe(image);
+ });
